0f78ef3e654ef729e87bd6e8c4175fc6ed272fcc
Gilbert Alonso Huarcaya Matias <you@example.com>
2025-10-09 17:45:25 -0500

    feat(components): add new PrimeVue components and update existing component registrations

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import "primeicons/primeicons.css";
 import Material from "@primeuix/themes/material";
 import PrimeVue from "primevue/config";
 import {
+  Avatar,
+  Badge,
   Button,
+  Calendar,
   Card,
   Checkbox,
   Column,
@@ -17,6 +20,7 @@ import {
   Dialog,
   DialogService,
   Drawer,
+  Dropdown,
   FileUpload,
   FloatLabel,
   IconField,
@@ -24,16 +28,23 @@ import {
   InputNumber,
   InputText,
   Menu,
+  ProgressBar,
+  ProgressSpinner,
   Rating,
   Row,
   Select,
   SelectButton,
+  TabPanel,
+  TabView,
   Tag,
   Textarea,
   Toast,
   ToastService,
   Toolbar,
   Tooltip,
+  AutoComplete,
+  Chip,
+  Message,
 } from "primevue";
 import router from "./router/router.js";
 import pinia from "./pinia.js";
@@ -45,14 +56,17 @@ createApp(App)
   .use(ConfirmationService)
   .use(DialogService)
   .use(ToastService)
-  .component("pv-avatar", Button)
+  .component("pv-avatar", Avatar)
+  .component("pv-badge", Badge)
   .component("pv-button", Button)
+  .component("pv-calendar", Calendar)
   .component("pv-card", Card)
   .component("pv-column", Column)
   .component("pv-confirm-dialog", ConfirmDialog)
   .component("pv-checkbox", Checkbox)
   .component("pv-data-table", DataTable)
   .component("pv-dialog", Dialog)
+  .component("pv-dropdown", Dropdown)
   .component("pv-select", Select)
   .component("pv-select-button", SelectButton)
   .component("pv-file-upload", FileUpload)
@@ -62,13 +76,21 @@ createApp(App)
   .component("pv-input-text", InputText)
   .component("pv-input-number", InputNumber)
   .component("pv-menu", Menu)
+  .component("pv-progressbar", ProgressBar)
+  .component("pv-progressspinner", ProgressSpinner)
   .component("pv-rating", Rating)
   .component("pv-row", Row)
   .component("pv-drawer", Drawer)
+  .component("pv-tabpanel", TabPanel)
+  .component("pv-tabview", TabView)
   .component("pv-tag", Tag)
   .component("pv-textarea", Textarea)
   .component("pv-toolbar", Toolbar)
   .component("pv-toast", Toast)
+  .component("pv-inputtext", InputText)
+  .component("pv-autocomplete", AutoComplete)
+  .component("pv-message", Message)
+  .component("pv-chip", Chip)
   .directive("tooltip", Tooltip)
   .use(router)
   .use(pinia)
