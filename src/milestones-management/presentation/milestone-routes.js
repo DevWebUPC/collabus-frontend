7b1f405f4ab93aa48401aa914870e2347ae2defd
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-20 02:15:09 -0500

    feat(milestones): add new route to redirection to view detail of milestone

--- src/milestones-management/presentation/milestone-routes.js
+++ src/milestones-management/presentation/milestone-routes.js
@@ -1,6 +1,9 @@
 // milestones-management/presentation/milestone-routes.js
 const MilestoneCreateForm = () => import('../presentation/views/MilestoneCreateForm.vue')
 const ProjectMilestonesView = () => import('../presentation/views/ProjectMilestonesView.vue')
+const MilestoneDetailView = () => import('../presentation/views/MilestoneDetailView.vue')
+
+// 👇 Asegúrate de que esto sea un export named
 export const milestoneRoutes = [
     {
         path: '/projects/:projectId/milestones/create',
@@ -17,6 +20,15 @@ export const milestoneRoutes = [
         component: ProjectMilestonesView,
         meta: {
             title: 'Milestones del Proyecto',
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/projects/:projectId/milestones/:milestoneId',
+        name: 'milestone-detail', // 👈 Este es el nombre que usas
+        component: MilestoneDetailView,
+        meta: {
+            title: 'Detalle del Hito',
             requiresAuth: true
         }
     }
