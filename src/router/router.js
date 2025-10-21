068f76ee53955a3bee4690d6c726ec8bc0e1b60a
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-21 00:40:57 -0500

    refactor(router): delete notification path

--- src/router/router.js
+++ src/router/router.js
@@ -10,7 +10,6 @@ import { projectRoutes } from "../projects/presentation/projects-routes.js";
 import { profileRoutes } from "../profile-management/presentation/profile-routes.js";
 import {taskRoutes} from "../task-management/presentation/task-routes.js";
 import {milestoneRoutes} from "../milestones-management/presentation/milestone-routes.js";
-import Notifications from "../shared/presentation/views/notifications-view.vue";
 
 // Import dinámico para page not found
 const pageNotFound = () =>
@@ -24,7 +23,6 @@ const routes = [
     { path: "/create-account", name: "create-account", component: Onboarding, meta: { title: "Create Account" } },
     { path: "/home", name: "home", component: Home, meta: { title: "Home" } },
     { path: "/collaborators", name: "collaborators", component: Collaborator, meta: { title: "Collaborators" } },
-    { path: "/notifications", name: "notifications", component: Notifications, meta: { title: "Notifications" } },
     { path: "/recovery-password", name: "recovery-password", component: RecoveryPassword, meta: { title: "Recovery Password" } },
     { path: "/:pathMatch(.*)*", name: "not-found", component: pageNotFound, meta: { title: "Page Not Found" } },
 
